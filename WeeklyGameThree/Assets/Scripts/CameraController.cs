c9c85beba5d2b422bedfe1527c888da58632d40f
ahg95 <ansgar.glup@gmx.de>
2024-02-23 17:32:04 +0100

    Adjusted camera controller to get movement bound values from GameVariable assets instead of a collider

--- WeeklyGameThree/Assets/Scripts/CameraController.cs
+++ WeeklyGameThree/Assets/Scripts/CameraController.cs
@@ -11,7 +11,12 @@ public class CameraController : MonoBehaviour
     [SerializeField]
     Transform _target;
 
-    public BoxCollider2D _RoomCollider;
+    [Header("Variables")]
+    [SerializeField]
+    Vector2Variable _minBounds;
+
+    [SerializeField]
+    Vector2Variable _maxBounds;
 
     [Header("Parameters")]
     [SerializeField]
@@ -27,10 +32,14 @@ public class CameraController : MonoBehaviour
 
     float _cameraZ;
     Vector3 _smoothDampVelocity;
+    bool _snapToTarget;
+
 
     private void Awake()
     {
         _cameraZ = _camera.transform.position.z;
+
+        SnapToTarget();
     }
 
     void LateUpdate()
@@ -43,24 +52,33 @@ public class CameraController : MonoBehaviour
 
         // - Adjust the target position such that the camera viewport will stay within the room bounds,
         // or such that it centers the room staying within the bounds of the room is impossible
-        var bounds = _RoomCollider.bounds;
         var halfViewport = new Vector2(_camera.orthographicSize * _camera.aspect, _camera.orthographicSize);
 
         // - - Adjust vertical position
-        var topOverlap = Mathf.Max(0, targetPosition.y + halfViewport.y - bounds.max.y);
-        var bottomOverlap = Mathf.Max(0, bounds.min.y - (targetPosition.y - halfViewport.y));
+        var topOverlap = Mathf.Max(0, targetPosition.y + halfViewport.y - _maxBounds.RuntimeValue.y);
+        var bottomOverlap = Mathf.Max(0, _minBounds.RuntimeValue.y - (targetPosition.y - halfViewport.y));
         var verticalMove = bottomOverlap - topOverlap;
 
         // - - Adjust horizontal position
-        var rightOverlap = Mathf.Max(0, targetPosition.x + halfViewport.x - bounds.max.x);
-        var leftOverlap = Mathf.Max(0, bounds.min.x - (targetPosition.x - halfViewport.x));
+        var rightOverlap = Mathf.Max(0, targetPosition.x + halfViewport.x - _maxBounds.RuntimeValue.x);
+        var leftOverlap = Mathf.Max(0, _minBounds.RuntimeValue.x - (targetPosition.x - halfViewport.x));
         var horizontalMove = leftOverlap - rightOverlap;
 
         targetPosition += Vector3.up * verticalMove + Vector3.right * horizontalMove;
 
 
 
-        // - Smoothly move to the target position
-        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref _smoothDampVelocity, _smoothTime, _maxSpeed, Time.deltaTime);
+        // - Move to the target position, either instantly or smoothly
+        if (_snapToTarget)
+        {
+            transform.position = targetPosition;
+            _snapToTarget = false;
+        } else
+            transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref _smoothDampVelocity, _smoothTime, _maxSpeed, Time.deltaTime);
+    }
+
+    public void SnapToTarget()
+    {
+        _snapToTarget = true;
     }
 }
